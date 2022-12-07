f9779754f01fd9d5c841d19561f0f8541e05472d
xVildis <viljami.sillanpaa@edu.tampere.fi>
2022-12-07 15:09:19 +0200

    remove redundant struct

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -7,30 +7,6 @@ enum RPS {
     S
 }
 
-struct Round {
-    enemy: RPS,
-    you: RPS
-}
-
-impl Round {
-    fn new(enemy: &str, you: &str) -> Round {
-        Round {
-            enemy: match enemy {
-                "A" => RPS::R,
-                "B" => RPS::P,
-                "C" => RPS::S,
-                _ => unreachable!("invalid input")
-            },
-            you: match you {
-                "X" => RPS::R,
-                "Y" => RPS::P,
-                "Z" => RPS::S,
-                _ => unreachable!("invalid input")
-            }
-        }
-    }
-}
-
 fn main() {
     let mut buffer = String::new();
     let mut stdin = io::stdin();
@@ -47,23 +23,34 @@ fn main() {
     for line in buffer.lines() {
         if let Some((enemy, you)) = line.split_once(' ') {
             // handle command, options
-            let round = Round::new(enemy, you);
 
-            println!("enemy {} you {}", round.enemy as i32, round.you as i32);
+            println!("enemy {} you {}", enemy, you);
 
-            let mut round_points = round.you as i32;
+            let enemy_hand = match enemy {
+                "A" => RPS::R,
+                "B" => RPS::P,
+                "C" => RPS::S,
+                _ => unreachable!("invalid enemy hand")
+            };
 
-            if round.enemy == round.you {
+            let your_hand = match you {
+                "X" => RPS::R,
+                "Y" => RPS::P,
+                "Z" => RPS::S,
+                _ => unreachable!("invalid hand")
+            };
+
+            let mut round_points = your_hand as i32;
+
+            if enemy_hand == your_hand {
                 round_points += 3;
-            } else if (round.enemy == RPS::R && round.you == RPS::P) 
-                   || (round.enemy == RPS::P && round.you == RPS::S)
-                   || (round.enemy == RPS::S && round.you == RPS::R) {
+            } else if (enemy_hand == RPS::R && your_hand == RPS::P) 
+                   || (enemy_hand == RPS::P && your_hand == RPS::S)
+                   || (enemy_hand == RPS::S && your_hand == RPS::R) {
                 round_points += 6;
             }
 
             total_points += round_points;
-        } else {
-            unreachable!("invalid input");
         }
     }
     println!("total points: {total_points}");
